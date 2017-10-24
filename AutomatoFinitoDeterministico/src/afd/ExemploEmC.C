cff71afa648dc1d0cd18c3c80c8d1e894226dbe9
Anderson Ferreira Canel <32991782+AndersonFCanel@users.noreply.github.com>
2017-10-24 14:30:25 -0200

    Create ExemploEmC.C

---
+++ AutomatoFinitoDeterministico/src/afd/ExemploEmC.C
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#define TAM 100
+
+void qInicio ( char palavra[TAM]);
+void q0( int contador, char palavra[TAM]);
+void q1( int contador, char palavra[TAM]);
+void q2( int contador, char palavra[TAM]);
+void q3( int contador, char palavra[TAM]);
+void q4( int contador, char palavra[TAM]);
+void q5( int contador, char palavra[TAM]);
+void q6( int contador, char palavra[TAM]);
+void q7( int contador, char palavra[TAM]);
+void q8( int contador, char palavra[TAM]);
+void q9( int contador, char palavra[TAM]);
+void qFim();
+void qErro();
+
+int main(){
+    char palavra[TAM] = "for"; //casos de comprovação: 
+    qInicio(palavra);
+    
+    return 0;
+}
+
+void qInicio ( char palavra[TAM] ){
+    int contador = 0;
+    q0( contador, palavra );
+}
+
+void q0( int contador, char palavra[TAM] ){
+    if ( contador < TAM ){
+        if ( palavra[contador] == 'f' ){
+           q1( ++contador, palavra ); 
+
+        }else if (palavra[contador] == 'c'){
+           q8( ++contador, palavra ); 
+
+        }else{
+            qErro();
+        }
+
+    }
+}
+
+void q1( int contador, char palavra[TAM] ){
+    if ( contador < TAM ){
+        if ( palavra[contador] == 'l' ){
+          q2( ++contador, palavra ); 
+
+        }else if (palavra[contador] == 'o'){
+           q3( ++contador, palavra ); 
+
+        }else{
+            qErro();
+        }
+
+    }
+}
+
+void q2( int contador, char palavra[TAM] ){
+    if ( palavra[contador] == 'o' ) {
+        q5 ( ++contador, palavra );
+        
+    } else {
+        qErro();
+    }
+}
+
+void q3( int contador, char palavra[TAM] ){
+    if ( palavra[contador] == 'r' ) {
+        q4 ( ++contador, palavra );
+        
+    } else {
+        qErro();
+    }
+}
+
+void q4( int contador, char palavra[TAM] ){
+    qFim();
+}
+
+void q5( int contador, char palavra[TAM] ){
+    if ( palavra[contador] == 'a' ) {
+        q6 ( ++contador, palavra );
+        
+    } else {
+        qErro();
+    }
+}
+
+void q6( int contador, char palavra[TAM] ){
+    if ( palavra[contador] == 't' ) {
+        q7 ( ++contador, palavra );
+        
+    } else {
+        qErro();
+    }
+}
+
+void q7( int contador, char palavra[TAM] ){
+    qFim();
+}
+
+void q8( int contador, char palavra[TAM] ){
+    if ( contador < TAM ){
+        if ( palavra[contador] == 'h' ){
+          q9( ++contador, palavra ); 
+
+        }else{
+            qErro();
+        }
+
+    }
+}
+
+void q9( int contador, char palavra[TAM] ){
+    if ( palavra[contador] == 'a' ) {
+        q3 ( ++contador, palavra );
+        
+    } else {
+        qErro();
+    }
+}
+
+
+void qErro(){
+    printf("Palavra regeitada pelo automata!\n");
+}
+
+void qFim(){
+    printf("Palavra Aceita pelo automata!\n");
+}
