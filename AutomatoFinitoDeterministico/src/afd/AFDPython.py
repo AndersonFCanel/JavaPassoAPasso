b7dcda02131191f38d03e0ce2a40a9ab0f3fa332
Anderson Ferreira Canel <32991782+AndersonFCanel@users.noreply.github.com>
2017-10-26 11:58:31 -0200

    Update AFDPython.py

--- AutomatoFinitoDeterministico/src/afd/AFDPython.py
+++ AutomatoFinitoDeterministico/src/afd/AFDPython.py
@@ -1,22 +1,30 @@
-#https://wiki.python.org.br/AprendaProgramar#Aprenda_a_programar
 #https://repl.it/languages/python
-#Só use len() quando você precisar saber:
-#Quantos caracteres tem numa string;
-#Quantos itens estão presentes numa lista, tupla, ou set;
-#Quantas chaves existem num dicionário;
-#Quantos itens existem em qualquer objeto iterável.
+#https://wiki.python.org.br/AprendaProgramar#Aprenda_a_programar
 
-print '**********Automatos**********'
+print '--------------------------------------'
+print '****AUTOMATO FINITO DETERMINISTICO****'
+print '--------------------------------------\n\n'
+
 alfabeto = raw_input('Digite um alfabeto: ')
-alflen = len(alfabeto)
+'''
+Pegando o modulo desse alfabelo
+'''
+alflen = len(alfabeto) 
+
 resposta = raw_input('Digite a quantidade de estados: ')
+'''resposta é string tem que haver uma conversão'''
 estados = int(resposta)
-parte={}
+
+parte={} 
 vai={}
 le={}
 estadosf={}
 i=0
-	
+'''
+p é incrementado dntro da faixa de 0 ao tamanho 
+de alflen
+
+'''
 for p in range(0, alflen):
 	for j in range(0, estados):
 		#print 'q%i' %j + ' le %c vai para: ' %alfabeto[p]
@@ -30,30 +38,34 @@ qestadosf = int (raw_input ('Quantos estados finais: '))
 print 'digite os %i estados finais' %qestadosf
 for p in range(0, qestadosf):
 	estadosf[p] = int(raw_input ('q'))
-palavra = raw_input('digite a palavra: ')
-palavralen = len(palavra)
-estadoa = estadoi
-for p in range(0, palavralen):
-	mudou=0
-	j=0
-	for i in range(0, estados*alflen):
-		if (palavra[p] == le[i]) and (estadoa == parte[i]):
-		#if (palavra[p] == alfabeto[j]):
-			aux=vai[i]
-			mudou=1
+
+resp = raw_input('\n\nQuantas Palavras irá testar? ')
+cad= int(resp)
+for p in range(cad)	:     #
+	palavra = raw_input('digite a palavra: ')
+	palavralen = len(palavra)
+	estadoa = estadoi
+	for p in range(0, palavralen):
+		mudou=0
+		j=0
+		for i in range(0, estados*alflen):
+			if (palavra[p] == le[i]) and (estadoa == parte[i]):
+			#if (palavra[p] == alfabeto[j]):
+				aux=vai[i]
+				mudou=1
+				break
+		if mudou == 1:
+			print 'de q%i ' %estadoa + 'leu %c ' %palavra[p] + 'foi para q%i ' %aux
+			estadoa=aux
+		else:
+			print 'nao tem regra para esse simbolo'
+			print 'o programa sera encerrado!!!'
 			break
-	if mudou == 1:
-		print 'de q%i ' %estadoa + 'leu %c ' %palavra[p] + 'foi para q%i ' %aux
-		estadoa=aux
+	teste = 0
+	for i in range(0, qestadosf):
+		if estadoa == estadosf[i]:
+			teste = 1
+	if teste == 1:
+		print '\nPalavra considerada valida\n\n'
 	else:
-		print 'nao tem regra para esse simbolo'
-		print 'o programa sera encerrado!!!'
-		break
-teste = 0
-for i in range(0, qestadosf):
-	if estadoa == estadosf[i]:
-		teste = 1
-if teste == 1:
-	print 'A palavra eh valida'
-else:
-	print 'A palavra nao eh valida'
+		print '\nPalavra nao considerada valida\n\n'
